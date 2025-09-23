a6f7a622bb6d4aae80cb45f1b3e66e1fd339c741
Evgenii Astapushkin <lebannehn.karnimirie@gmail.com>
2025-09-23 23:37:33 +0200

    [feat] Direction feature

--- package/stalinSort/index.ts
+++ package/stalinSort/index.ts
@@ -1,14 +1,21 @@
 /**
  * Sort an array of numbers with stalin sort.
- * @param {number[]} values
+ * @param {number[]} values an array of numbers
+ * @param {boolean} isAscending ascending or descending order, true by default
  */
-export function stalinSort(values: number[] = []): number[] {
+export function stalinSort(values: number[] = [], isAscending = true): number[] {
 	if (!values.length) {
 		return values;
 	}
 
+	function checkDirection(previous: number[], next: number): boolean {
+		return isAscending
+			? next >= previous[previous.length - 1]
+			: next <= previous[previous.length - 1];
+	}
+
 	return values.reduce(
-		(previous: number[], next: number, index: number) => !index || next >= previous[previous.length - 1]
+		(previous: number[], next: number, index: number) => !index || checkDirection(previous, next)
 			? [...previous, next]
 			: previous,
 		[]
