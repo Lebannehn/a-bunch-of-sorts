a1e2cac299039e965fb401a31df8cd3af9f8e3ae
Evgenii Astapushkin <lebannehn.karnimirie@gmail.com>
2025-09-28 22:23:29 +0200

    [feat] Unique option for stalinsort

--- package/stalinSort/index.ts
+++ package/stalinSort/index.ts
@@ -1,21 +1,24 @@
 /**
  * Sort an array of numbers with stalin sort.
+ * Stalin sort is a sorting algorithm that removes elements from an array that are out of order.
+ *
  * @param {number[]} values an array of numbers
- * @param {boolean} isAscending ascending or descending order, true by default
+ * @param {boolean} isAscendingOrder ascending or descending order, true by default
+ * @param {boolean} isUnique if true, the array will contain unique values
  */
-export function stalinSort(values: number[] = [], isAscending = true): number[] {
+export function stalinSort(values: number[] = [], isAscendingOrder = true, isUnique = false): number[] {
 	if (!values.length) {
 		return values;
 	}
 
-	function checkDirection(previous: number[], next: number): boolean {
-		return isAscending
-			? next >= previous[previous.length - 1]
-			: next <= previous[previous.length - 1];
+	function mercy(previous: number[], next: number): boolean {
+		return isAscendingOrder
+			? isUnique ? next > previous[previous.length - 1] : next >= previous[previous.length - 1]
+			: isUnique ? next < previous[previous.length - 1] : next <= previous[previous.length - 1];
 	}
 
 	return values.reduce(
-		(previous: number[], next: number, index: number) => !index || checkDirection(previous, next)
+		(previous: number[], next: number, index: number) => !index || mercy(previous, next)
 			? [...previous, next]
 			: previous,
 		[]
