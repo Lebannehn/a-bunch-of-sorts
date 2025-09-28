00da45ade06cb498423dfdbbcbea4d25a9eea5df
Evgenii Astapushkin <lebannehn.karnimirie@gmail.com>
2025-09-28 22:49:10 +0200

    [feat] Moved all arguments to the options argument

--- package/stalinSort/index.ts
+++ package/stalinSort/index.ts
@@ -1,20 +1,32 @@
+type TerrorOptions = {
+	/**
+	 * Sort by ascending order or not
+	 * @default true
+	 */
+	ascendingOrder: boolean,
+	/**
+	 * Remove duplicates
+	 * @default false
+	 */
+	uniqueValues: boolean,
+};
+
 /**
  * Sort an array of numbers with stalin sort.
  * Stalin sort is a sorting algorithm that removes elements from an array that are out of order.
  *
  * @param {number[]} values an array of numbers
- * @param {boolean} isAscendingOrder ascending or descending order, true by default
- * @param {boolean} isUnique if true, the array will contain unique values
+ * @param {TerrorOptions} options a set of options
  */
-export function stalinSort(values: number[] = [], isAscendingOrder = true, isUnique = false): number[] {
+export function stalinSort(values: number[] = [], options: TerrorOptions): number[] {
 	if (!values.length) {
 		return values;
 	}
 
 	function mercy(previous: number[], next: number): boolean {
-		return isAscendingOrder
-			? isUnique ? next > previous[previous.length - 1] : next >= previous[previous.length - 1]
-			: isUnique ? next < previous[previous.length - 1] : next <= previous[previous.length - 1];
+		return options.ascendingOrder
+			? options.uniqueValues ? next > previous[previous.length - 1] : next >= previous[previous.length - 1]
+			: options.uniqueValues ? next < previous[previous.length - 1] : next <= previous[previous.length - 1];
 	}
 
 	return values.reduce(
