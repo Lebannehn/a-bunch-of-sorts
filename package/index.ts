30120048be9e684acd2453bf7e7d0375820f279a
Evgenii Astapushkin <lebannehn.karnimirie@gmail.com>
2026-04-01 01:54:12 +0200

    [feat] intelligence/divine sort

--- package/index.ts
+++ package/index.ts
@@ -1,2 +1,3 @@
 export { stalinSort } from './stalinSort';
 export { miracleSort } from './miracleSort';
+export { intelligenceSort } from './intelligenceSort';
---
+++ package/intelligenceSort/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Sort an array of numbers with intelligence sort.
+ * Intelligence sort is a sorting algorithm that assumes that the input values are already sorted by divine intervention, or it was intended that way by a higher power.
+ * Deus Vult.
+ * O(1) time complexity.
+ * O(1) space complexity.
+ *
+ * @param {number[]} values
+ */
+export const intelligenceSort = (
+	values: number[],
+) => {
+	return values;
+};
